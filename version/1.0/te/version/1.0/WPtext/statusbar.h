2e8fc00fb31f214dc7bb66112c7bb989a8e67a35
lorel <fyfdhgfgg09@gmail.com>
2025-09-15 05:08:38 +0300

    v 1.0 first commit :)

--- version/1.0/te/version/1.0/WPtext/statusbar.h
+++
@@ -1,9 +0,0 @@
-#ifndef STATUSBAR_H
-#define STATUSBAR_H
-
-#include <string>
-#include <ncursesw/ncurses.h>
-
-int status_load(const std::string& filename, const std::string& status, const std::string& version, int maxx);
-
-#endif
